8942a4a8b5db2e54ed64d98d56a1fd469e2e0345
Java-Blick <java.blick@gmail.com>
2017-07-03 22:19:59 +0200

    Init

---
+++ src/main/java/com/java/blick/dates/Dates.java
@@ -0,0 +1,12 @@
+package com.java.blick.dates;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Dates {
+
+	public static Timestamp convert(Date date) {
+		return null;
+	}
+
+}
