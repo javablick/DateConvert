8deb344de6ea27292144bc7fe3dfbae93f75c14a
Michael <java.blick@gmail.com>
2017-07-04 11:17:05 +0200

    First converter methods

--- src/main/java/com/java/blick/dates/Dates.java
+++ src/main/java/com/java/blick/dates/Dates.java
@@ -3,10 +3,20 @@ package com.java.blick.dates;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import com.java.blick.dates.factories.LocalDateTimeFactory;
+
 public class Dates {
 
-	public static Timestamp convert(Date date) {
-		return null;
+	public static Timestamp toTimestamp(Date date) {
+		if (date == null)
+			return null;
+		return new Timestamp(date.getTime());
+	}
+
+	public static LocalDateTimeFactory toLocalDateTime(Date date) {
+		if (date == null)
+			return null;
+		return new LocalDateTimeFactory(date);
 	}
 
 }
--- src/main/java/com/java/blick/dates/factories/LocalDateTimeFactory.java
+++ src/main/java/com/java/blick/dates/factories/LocalDateTimeFactory.java
@@ -1,5 +1,23 @@
 package com.java.blick.dates.factories;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 public class LocalDateTimeFactory {
+
+	private Date date;
+
+	public LocalDateTimeFactory(Date date) {
+		this.date = date;
+	}
+
+	public LocalDateTime withDefaultZone() {
+		return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+	}
+
+	public LocalDateTime withZone(ZoneId zone) {
+		return LocalDateTime.ofInstant(date.toInstant(), zone);
+	}
 
 }
